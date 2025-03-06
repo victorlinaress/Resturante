a5b339797be154439f9f8294960d8ce3401c3567
unknown <victorbr8@outlook.com>
2025-03-06 16:08:48 -0300

    services item

--- backend/src/src/services/itemCardapioService.ts
+++ backend/src/src/services/itemCardapioService.ts
@@ -1,5 +1,6 @@
 import ItemCardapioInput from "../interfaces/Input/ItemCardapioInput";
 import ItemCardapioOutput from "../interfaces/Outputs/ItemCardapioOutput";
+import itemCardapio from "../models/ItemCardapio";
 
 async function create(
   itemCardapioInput: ItemCardapioInput
@@ -21,3 +22,39 @@ async function create(
 
   return itemCardapioOutput;
 }
+
+async function getAllForRestaurant(
+  idRestaurant: number
+): Promise<ItemCardapioOutput[]> {
+  const itensCardapio: itemCardapio[] = await prisma.itens_cardapio.findMany({
+    where: { status: true, idRestaurant: idRestaurant },
+  });
+
+  if (itensCardapio.length === 0) {
+    throw new NotFoundError();
+  }
+
+  const itemCardapioOutput: ItemCardapioOutput[] = await Promise.all(
+    itensCardapio.map(async (itemCardapio) => {
+      const restauranteOutput = await restauranteService.getById(
+        itemCardapio.idRestaurante
+      );
+      return {
+        id: itemCardapio.id,
+        restauranteOutput: restauranteOutput, // detalhes do restaurante
+        nome: itemCardapio.nome,
+        descricao: itemCardapio.descricao,
+        preco: itemCardapio.preco,
+        fotoUrl: itemCardapio.fotoUrl,
+      };
+    })
+  );
+
+  return itemCardapioOutput;
+}
+
+async function getById(id: number): Promise<ItemCardapioOutput> {
+    const itemCardapio: itemCardapio | null = await prisma.itens_cardapio.findUnique({
+      where: { id, status: true },
+    });
+  
