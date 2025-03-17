eb122af80465657db3b379b306e3d8cfaa600ebb
unknown <victorbr8@outlook.com>
2025-03-16 23:50:05 -0300

    controllers

--- backend/src/src/controllers/itemPedidoController.ts
+++ backend/src/src/controllers/itemPedidoController.ts
@@ -4,16 +4,25 @@ import itemPedidoService from "../services/itemPedidoService";
 import ItemPedidoOutput from "../interfaces/Outputs/ItemPedidoOutput";
 
 async function create(req: Request, res: Response) {
-    const itemPedidoInput: ItemPedidoInput = req.body;
+  const itemPedidoInput: ItemPedidoInput = req.body;
 
-    try {
-        const itemPedidoOutput: ItemPedidoOutput = await itemPedidoService.create(itemPedidoInput);
-        res.status(201).json(itemPedidoOutput);
-    } catch (error) {
-        res.status(500).json({ error: "Erro ao criar item do pedido", descricao: error });
-    }
+  try {
+    const itemPedidoOutput: ItemPedidoOutput = await itemPedidoService.create(
+      itemPedidoInput
+    );
+    res.status(201).json(itemPedidoOutput);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Erro ao criar item do pedido", descricao: error });
+  }
+}
+
+async function getAllForPedido(req:Request, rep:Response) {
+  const {idPedido} = req.params
+  
 }
 
 export default {
-    create,
+  create,
 };
